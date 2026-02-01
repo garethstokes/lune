6139136e5310fd1fd8e019bf0af8b0b0a413cafb
gareth <gareth.stokes@paidright.io>
2026-02-01 21:37:19 +1100

    feat: add type validation pass

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,59 @@
 module Main where
 
 import System.Environment (getArgs)
+import System.Exit (exitFailure)
 import Lune.Desugar (desugarModule)
 import Lune.Parser (parseFile)
+import Lune.Validate (validateModule)
 
 main :: IO ()
 main = do
   args <- getArgs
+  case parseArgs args of
+    Left msg ->
+      putStrLn msg
+    Right opts ->
+      run opts
+
+data Options = Options
+  { optPath :: FilePath
+  , optDesugar :: Bool
+  , optValidate :: Bool
+  }
+
+parseArgs :: [String] -> Either String Options
+parseArgs args =
   case args of
-    ["--parse", path] ->
-      runParse path
-
-    ["--desugar", path] ->
-      runDesugar path
-
     [path] ->
-      runParse path
-
+      Right Options {optPath = path, optDesugar = False, optValidate = False}
     _ ->
-      putStrLn "Usage: lune [--parse|--desugar] <file.lune>"
+      case reverse args of
+        path : flagsRev
+          | "--" `isPrefixOf` path ->
+              Left usage
+          | otherwise ->
+              let flags = reverse flagsRev
+                  desugar = "--desugar" `elem` flags
+                  validate = "--validate" `elem` flags
+                  unknown = filter (`notElem` ["--parse", "--desugar", "--validate"]) flags
+               in if null unknown
+                    then Right Options {optPath = path, optDesugar = desugar, optValidate = validate}
+                    else Left usage
+        _ ->
+          Left usage
+  where
+    usage = "Usage: lune [--parse|--desugar] [--validate] <file.lune>"
+    isPrefixOf prefix str = take (length prefix) str == prefix
 
-runParse :: FilePath -> IO ()
-runParse path = do
-  result <- parseFile path
-  print result
-
-runDesugar :: FilePath -> IO ()
-runDesugar path = do
-  result <- parseFile path
-  print (desugarModule result)
+run :: Options -> IO ()
+run opts = do
+  surface <- parseFile (optPath opts)
+  let mod' = if optDesugar opts then desugarModule surface else surface
+  if optValidate opts
+    then case validateModule mod' of
+      Left err -> do
+        putStrLn (show err)
+        exitFailure
+      Right () ->
+        putStrLn "OK"
+    else print mod'
---
+++ src/Lune/Validate.hs
@@ -0,0 +1,159 @@
+module Lune.Validate
+  ( ValidateError (..)
+  , validateModule
+  ) where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Lune.Syntax
+
+data ValidateError
+  = TypeConstructorArityMismatch Text Int Int
+  | AppliedArrowType
+  | AppliedRecordType
+  | DuplicateRecordTypeFields [Text]
+  deriving (Eq)
+
+instance Show ValidateError where
+  show err =
+    case err of
+      TypeConstructorArityMismatch name expected got ->
+        T.unpack name
+          <> " expects "
+          <> show expected
+          <> " type arguments"
+          <> (if expected == got then "" else " (got " <> show got <> ")")
+      AppliedArrowType ->
+        "Cannot apply a function type"
+      AppliedRecordType ->
+        "Cannot apply a record type"
+      DuplicateRecordTypeFields fields ->
+        "Record type has duplicate field(s): " <> T.unpack (T.intercalate ", " fields)
+
+validateModule :: Module -> Either ValidateError ()
+validateModule m = do
+  let env = builtinArities <> declaredArities (modDecls m)
+  mapM_ (validateDecl env) (modDecls m)
+
+type ArityEnv = [(Text, Int)]
+
+builtinArities :: ArityEnv
+builtinArities =
+  [ ("Int", 0)
+  , ("Bool", 0)
+  , ("String", 0)
+  , ("Unit", 0)
+  , ("List", 1)
+  , ("Maybe", 1)
+  , ("Result", 2)
+  , ("IO", 1)
+  ]
+
+declaredArities :: [Decl] -> ArityEnv
+declaredArities =
+  foldr go []
+  where
+    go decl acc =
+      case decl of
+        DeclType name vars _ ->
+          (name, length vars) : acc
+        DeclTypeAlias name vars _ ->
+          (name, length vars) : acc
+        DeclNewtype name vars _ _ ->
+          (name, length vars) : acc
+        _ ->
+          acc
+
+validateDecl :: ArityEnv -> Decl -> Either ValidateError ()
+validateDecl env decl =
+  case decl of
+    DeclTypeSig _ ty ->
+      validateType env ty
+    DeclType _ _ ctors ->
+      mapM_ (validateTypeCtor env) ctors
+    DeclTypeAlias _ _ body ->
+      validateType env body
+    DeclNewtype _ _ _ ctorTy ->
+      validateType env ctorTy
+    DeclValue {} ->
+      Right ()
+
+validateTypeCtor :: ArityEnv -> TypeCtor -> Either ValidateError ()
+validateTypeCtor env (TypeCtor _ args) =
+  mapM_ (validateType env) args
+
+validateType :: ArityEnv -> Type -> Either ValidateError ()
+validateType env ty =
+  case ty of
+    TypeArrow dom cod -> do
+      validateType env dom
+      validateType env cod
+
+    TypeRecord fields -> do
+      validateRecordFields env fields
+
+    TypeApp {} ->
+      validateAppliedType env ty
+
+    TypeCon name ->
+      validateTypeConstructorArity env name 0
+
+    TypeVar _ ->
+      Right ()
+
+validateAppliedType :: ArityEnv -> Type -> Either ValidateError ()
+validateAppliedType env ty = do
+  let (headTy, args) = unapplyTypeApps ty
+  case headTy of
+    TypeArrow {} ->
+      Left AppliedArrowType
+    TypeRecord {} ->
+      Left AppliedRecordType
+    TypeCon name ->
+      validateTypeConstructorArity env name (length args)
+    _ ->
+      Right ()
+  mapM_ (validateType env) args
+
+validateTypeConstructorArity :: ArityEnv -> Text -> Int -> Either ValidateError ()
+validateTypeConstructorArity env name got =
+  case lookup name env of
+    Nothing ->
+      Right ()
+    Just expected ->
+      if expected == got
+        then Right ()
+        else Left (TypeConstructorArityMismatch name expected got)
+
+validateRecordFields :: ArityEnv -> [(Text, Type)] -> Either ValidateError ()
+validateRecordFields env fields = do
+  let names = map fst fields
+  case duplicates names of
+    [] -> Right ()
+    dups -> Left (DuplicateRecordTypeFields dups)
+  mapM_ (validateType env . snd) fields
+
+duplicates :: Eq a => [a] -> [a]
+duplicates =
+  go [] []
+  where
+    go _ dupes [] =
+      reverse dupes
+    go seen dupes (x : xs)
+      | x `elem` seen =
+          if x `elem` dupes
+            then go seen dupes xs
+            else go seen (x : dupes) xs
+      | otherwise =
+          go (x : seen) dupes xs
+
+unapplyTypeApps :: Type -> (Type, [Type])
+unapplyTypeApps =
+  go []
+  where
+    go args t =
+      case t of
+        TypeApp f x ->
+          go (x : args) f
+        _ ->
+          (t, args)
